cc66dc1f2776ca3bf74e8c46a9825104d9d1f99e
Sambhab Bose <bosesambhab2020@gmail.com>
2026-03-27 18:20:20 +0530

    setup FastAPI server with GET and POST debug endpoint

---
+++ backend/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class code_input(BaseModel):
+    code: str
+
+@app.get("/")
+def home():
+    return{
+        "message": "Welcome to backend",
+    }
+
+@app.post("/debug")
+def debug(data: code_input):
+    print(data)
+    return{
+        "message" :"Its Working!!",
+        "your code": data.code
+    }
+
+
